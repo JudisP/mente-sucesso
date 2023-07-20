bdf8159c89327857d19aaf106a3a7306ba756144
JudisP <judisson429@hotmail.com>
2023-07-20 17:47:18 -0300

    Adicionando o Main e mudanças no CSS

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -6,7 +6,9 @@ function App() {
 
   return (
     <>
-      <Construcao />
+      <main>
+        <Construcao />
+      </main>
     </>
   )
 }
--- src/pages/Construcao.jsx
+++ src/pages/Construcao.jsx
@@ -1,15 +1,19 @@
 // import { Container } from "reactstrap"
-import background from "../assets/backgroundConstrucao.png"
-
+import background from "../assets/backgroundConstrucao.png";
 
 export function Construcao() {
   return (
-      // <Container className="container-principal-construcao m-0 p-0">
-        <div className="background-construcao">
-        <img src={background} className="imagem" alt="Imagem de Fundo da Página de Construção" />
-        <h1 className="screen-xs">TESTE DE TEXTO</h1>
-        </div>
-      // </Container>
-  )
+    <>
+    <div className="container-manuntencao">
+      <section className="background-construcao">
+      <img
+        src={background}
+        className="imagem"
+        alt="Imagem de Fundo da Página de Construção"
+      />
+      <h1 className="screen-xs">TESTE DE TEXTO</h1>
+      </section>
+    </div>
+    </>
+  );
 }
-
