5ad06916d8932cb8a790dd094a852a5b192852da
JudisDev <judisson429@hotmail.com>
2023-07-19 21:32:55 -0300

    Alinhando o Background da página de Manutenção

--- src/pages/Construcao.jsx
+++ src/pages/Construcao.jsx
@@ -1,15 +1,15 @@
-import { Container } from "reactstrap"
+// import { Container } from "reactstrap"
 import background from "../assets/backgroundConstrucao.png"
 
 
 export function Construcao() {
   return (
-      <Container className="m-0 p-0">
+      // <Container className="container-principal-construcao m-0 p-0">
         <div className="background-construcao">
         <img src={background} className="imagem" alt="Imagem de Fundo da Página de Construção" />
         <h1 className="screen-xs">TESTE DE TEXTO</h1>
         </div>
-      </Container>
+      // </Container>
   )
 }
 
